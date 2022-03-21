bf0e936127b1bb635ffdc4484ed4738d0fd21089
gauge2009 <promvc@live.com>
2022-03-21 23:12:19 +0800

    3、4、5、6 第三层的写法过于冗长， 我们简化一下： 简化思路： 从 cmd 的输出管道 直连 缓冲区 bytes.Buffer后，你.Bytes()一下就有了二进制， 把这个二进制字节数组传递给 file.Write 就实现了cmd 输出写入文件的诉求：
    第四层： 将 cmd输出写入文件  Stdout->bytes.Buffer-->file.Write(ouputbuf1.Bytes())
    
    第五层：  用内存/系统的命名管道实现进程通信~os.Pipe() ~ 基于操作系统文件思维  22-3-13
    
    7.实现进程的信号通信
    （2022 年 3 月 13 日 成功基于proc.Signal(sig)向指定 pid发送信号关闭了印象笔记 ）
    
    8.go-parallelist-(1-元春)-多进程编程2-实现进程间的tcp通讯—手写网络服务器-分布式通信框架-22-3-19
    go-parallelist-(2-熙凤)-多线程编程-(2)《-chan 取出 和 chan《- 压入 22-3-20  on   gaugeMacbookProI9 22-3-21

--- 多线程编程/3.channelval.go
+++ 多线程编程/3.channelval.go
@@ -5,36 +5,40 @@ import (
 	"time"
 )
 
-var mapChan=make(chan map[string]int,1)//引用类型
+var mapChan = make(chan map[string]int, 1) //引用类型
 
-func main(){
-	syncChan:=make(chan struct{},2)
+func main() {
+	syncChan := make(chan struct{}, 2) // 该通道用于流水线同步【即：生产者和消费者都完成后，主线程再结束】
+
 	go func() {
 		for {
-			if elem ,ok:=<-mapChan;ok{
-				elem["count"]++
-			}else{
+			if elem, ok := <-mapChan; ok {
+				elem["zrz1898zrz的余额"]++
+			} else {
+
 				break
 			}
 		}
 		fmt.Println("stop 接收")
 
-		syncChan<- struct{}{}
+		syncChan <- struct{}{} //双小吱 压入：消费者通知线程
+
 	}()
 
 	go func() {
-		countmap:=make(map[string]int)
-		for i:=0;i<5;i++{
-			mapChan<-countmap//压入数据
+		countmap := make(map[string]int)
+		for i := 0; i < 5; i++ {
+			mapChan <- countmap //压入数据
 			time.Sleep(time.Microsecond)
-			fmt.Printf("countmap %v\n",countmap)
+			fmt.Printf("countmap %v万\n", countmap)
 		}
 		close(mapChan)
-		syncChan<- struct{}{}
+
+		syncChan <- struct{}{} //双小吱压入：生产者通知线程
+
 	}()
-	<-syncChan
-	<-syncChan
 
-
+	<-syncChan //双小吱等待读取
+	<-syncChan //双小吱等待读取
 
 }
